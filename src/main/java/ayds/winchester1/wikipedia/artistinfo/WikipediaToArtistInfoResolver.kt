64d479fa193ce7c6b940774f2043e15e13345ea7
Marco Valla <marcovalla2014@gmail.com>
2022-06-05 17:26:57 -0300

    Se corrigio la variable LOGO_URL agregando el prefijo https://

--- src/main/java/ayds/winchester1/wikipedia/artistinfo/WikipediaToArtistInfoResolver.kt
+++ src/main/java/ayds/winchester1/wikipedia/artistinfo/WikipediaToArtistInfoResolver.kt
@@ -8,7 +8,7 @@ private const val SNIPPET = "snippet"
 private const val SEARCH = "search"
 private const val PAGE_ID = "pageid"
 private const val QUERY = "query"
-private const val LOGO_URL= "upload.wikimedia.org/wikipedia/commons/8/8c/Wikipedia-logo-v2-es.png"
+private const val LOGO_URL= "https://upload.wikimedia.org/wikipedia/commons/8/8c/Wikipedia-logo-v2-es.png"
 
 interface WikipediaToArtistInfoResolver {
     fun getCardFromExternalData(serviceData: String?): WikipediaArtistInfo?
