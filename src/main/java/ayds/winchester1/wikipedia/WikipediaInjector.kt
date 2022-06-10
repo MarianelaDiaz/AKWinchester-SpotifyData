e27656a040f37c30a13c8bbda1294bd12506b024
Marco Valla <marcovalla2014@gmail.com>
2022-06-10 16:40:06 -0300

    Fixed doble injector

--- src/main/java/ayds/winchester1/wikipedia/WikipediaInjector.kt
+++ src/main/java/ayds/winchester1/wikipedia/WikipediaInjector.kt
@@ -1,7 +1,21 @@
 package ayds.winchester1.wikipedia
 
-import ayds.winchester1.wikipedia.artistinfo.WikipediaArtistInfoInjector
+import ayds.winchester1.wikipedia.artistinfo.*
+import ayds.winchester1.wikipedia.artistinfo.JsonToArtistInfoResolver
+import ayds.winchester1.wikipedia.artistinfo.WikipediaArtistServiceImpl
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object WikipediaInjector {
-    val wikipediaService: WikipediaService = WikipediaArtistInfoInjector.wikipediaService
+    private const val WIKIPEDIA_API_BASE_URL = "https://en.wikipedia.org/w/"
+    private val wikipediaAPIRetrofit = Retrofit.Builder()
+        .baseUrl(WIKIPEDIA_API_BASE_URL)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .build()
+    private val wikipediaArtistInfoAPI = wikipediaAPIRetrofit.create(WikipediaArtistInfoAPI::class.java)
+    private val wikipediaToArtistInfoResolver: WikipediaToArtistInfoResolver = JsonToArtistInfoResolver()
+    val wikipediaService: WikipediaService = WikipediaArtistServiceImpl(
+        wikipediaArtistInfoAPI,
+        wikipediaToArtistInfoResolver
+    )
 }
